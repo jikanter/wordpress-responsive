1f09c51f99792fbbaa3679a097b1113e0781d01b
Dan <art.starschreck@gmail.com>
2014-07-17 16:05:08 -0500

    Post seperator in 'the loop' / attachement.php simplified / css

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
 <header id="header" role="banner">
 <section id="branding">
 <div id="site-title"><?php if ( ! is_singular() ) { echo '<h1>'; } ?><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php esc_attr_e( get_bloginfo( 'name' ), 'blankslate' ); ?>" rel="home"><?php echo esc_html( get_bloginfo( 'name' ) ); ?></a><?php if ( ! is_singular() ) { echo '</h1>'; } ?></div>
-<div id="site-description"><?php bloginfo( 'description' ); ?></div>
+<!-- <div id="site-description"><?php bloginfo( 'description' ); ?></div> -->
 </section>
 </div>
 <nav id="menu" role="navigation">
