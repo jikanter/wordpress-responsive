5519e077390da180912825971dfd787686448610
Jordan Kanter <jikanter@gmail.com>
2014-07-23 14:11:13 -0500

    colophon (footer.php) cleanup. added boomshaka info

--- footer.php
+++ footer.php
@@ -14,7 +14,7 @@
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'boomshaka' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'boomshaka' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'boomshaka' ), 'boomshaka', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
+			<?php printf( __( 'Theme: %1$s by %2$s.', 'boomshaka' ), 'boomshaka', '<a href="http://github.com/jikanter/wordpress-responsive" rel="designer">Boomshaka Design</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
