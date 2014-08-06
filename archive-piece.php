b3cbac9d208fff9ed1e61308de57e660ddabe483
Dan <art.starschreck@gmail.com>
2014-08-06 12:39:55 -0500

    Using new classes in archive-piece.php

--- archive-piece.php
+++ archive-piece.php
@@ -82,13 +82,13 @@ get_header(); ?>
         // Start the Loop
         while ( have_posts() ) : the_post(); ?>
  
-        <article class="entry-content">
+        <article class="entry-content type-piece">
             <?php $attr = array(
                 'class' => "archive-image",
                 //'alt' => trim( strip_tags( $wp_postmeta->_wp_attachment_image_alt ) ),
             ); ?>
             <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail( 'thumbnail', $attr ); ?></a>
-            <h2 class="archive-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+            <!-- <h2 class="archive-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>-->
         </article>
         
               <?php endwhile; ?>
