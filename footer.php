3626d7095ae7e481958a0ea92cae6aa3fc93e14e
Dan <art.starschreck@gmail.com>
2014-10-23 20:06:51 -0500

    Footer formating change. Site branding and Boom branding.

--- footer.php
+++ footer.php
@@ -14,7 +14,8 @@
 		
 		<div class="site-info">
 		<p>Copyright &copy; <?php echo date("Y") ?> <?php bloginfo( 'name' ); ?>. <?php bloginfo( 'description' ); ?>. </p>
-			<?php printf( __( 'WordPress x %1$s = %2$s.', 'boomshaka' ), 'boomshaka', '<a href="http://boomshaka.starschreck.com/signup" rel="designer">Boomshakalaka</a>' ); ?>
+		Artist Websites by <a href="http://boomshaka.starschreck.com/signup">Boomshaka</a>. Show your work to the world.
+		
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
