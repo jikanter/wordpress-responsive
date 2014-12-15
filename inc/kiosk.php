dd6aa299821e351c11c6c622564474840872e01d
Jordan Kanter <jikanter@gmail.com>
2014-12-15 16:51:56 -0600

    added loop to kiosk.

--- inc/kiosk.php
+++ inc/kiosk.php
@@ -2,11 +2,26 @@
 <html>
   <head>
 <?php
-require_once("../")
-// load the pieces here and display via boomshaka-carousel
+require_once("../wp-includes/query.php");
+// remember, each post_type is a product (a woocommerce product)
+$args = array('post_type' => 'product');
+$pieces = new WP_Query($args);
 ?>
+<script src="../js/boomshaka.js" type="text/javascript"></script>
+<script src="../js/boomshaka-carousel.js" type="text/javascript"></script>
   </head>
   <body>
-    FULL SCREEN? CAN YOU HEAR ME MAJOR TOM?
+    <!-- FULL SCREEN? CAN YOU HEAR ME MAJOR TOM? -->
+    <?php
+    // do the loop here
+    if ( $pieces->have_posts() ) : 
+      while ( $pieces->have_posts() ) :
+        echo('<div>');
+          echo( get_the_thumbnail() );
+        echo('</div>');
+      endwhile;
+    endif;
+    ?>
+    
   </body>
 </html>
