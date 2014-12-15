d6d516b14bf022bcdf9a447d7817ca813de475be
Jordan Kanter <jikanter@gmail.com>
2014-12-15 12:30:07 -0600

    added some backend code to catch widget generators from front end

--- js/boomshaka-carousel.js
+++ js/boomshaka-carousel.js
@@ -1,5 +1,23 @@
-/** Boomshaka Materials */
 Boomshaka.Material.Carousel = function() { 
-  var Animation = {};
+  this.material = new Boomshaka.Material();
+  this.material.Layers = [new Layer(), null];
+  this.material.Animation = {};
+  this.material.native = null;
+  this.material.deviceid = 0; // browser
   
+  this.Animation.step = function(id, dx, dy, dz) { 
+    var elem = document.getElementById(id);
+    if ((!this.material.native) || (this.material.native == null)) {
+      elem.style.x = elem.style.x + dx + ' px';
+      elem.style.y = elem.style.y + dy + ' px';
+      elem.style.z = elem.style.z + dz + ' px';
+    }
+    else { 
+      // native
+      elem.x += dx;
+      elem.y += dy;
+      elem.z += dz;
+    }
+    return elem;
+  };
 };
--- js/boomshaka.js
+++ js/boomshaka.js
@@ -1,50 +1,69 @@
 // make sure boomshaka is loaded
-if (typeof Boomshaka == "undefined") { 
+if ((typeof Boomshaka == "undefined") || (BoomShaka == null)) { 
 	var Boomshaka = {
-	  init: function() { 
-	      return jQuery(document).ready(function() { 
-	        this.createBoomshakaCustomizer = function() {
-        		var boomshakaCustomizerContainer = document.createElement("li");
-        		boomshakaCustomizerContainer.setAttribute("class", "control-section accordion-section top");
-        		// set the id to boomshaka
-        		boomshakaCustomizerContainer = boomshakaContainer.attr("id", "accordion-section-title_boomshaka");
-        		boomshakaCustomizerTitle = document.createElement("h3");
-        		boomshakaCustomizerTitle.appendChild(document.createTextNode("Customize Gallery"));
-
-        		boomshakaCustomizerContentContainer = document.createElement("ul");
-        		boomshakaCustomizerContentContainer.setAttribute("class", "accordion-section-content");
-        		boomshakaCustomizerContentContainer.setAttribute("style", "display: block");
-
-        		// piece height
-        		boomshakaCustomizerHeightItem = document.createElement("li");
-        		boomshakaCustomizerHeightItem.setAttribute("id", "customize-control-piece-default-height");
-        		boomshakaCustomizerHeightItem.setAttribute("class", "customize-control customize-control-text");
-        		boomshakaCustomizerHeightLabel = document.createElement("label");
-        		boomshakaCustomizerHeightLabel.appendChild(document.createTextNode("Piece Default Height"));
-
-        		// piece width
-        		boomshakaCustomizerWidthItem = document.createElement("li");
-        		boomshakaCustomizerWidthItem.setAttribute("id", "customize-control-piece-default-weight");
-        		boomshakaCustomizerWidthItem.setAttribute("class", "customize-contol customize-control-text");
-        		boomshakaCustomizerWidthLabel = document.createElement("label");
-        		boomshakaCustomizerWidthLabel.appendChild(document.createTextNode("Piece Default Width"));
-
-        		// append the boomshaka items to the container
-        		boomshakaCustomizerContentContainer.appendChild(boomshakaCustomizerHeightItem);
-        		boomshakaCustomizerContentContainer.appendChild(boomshakaCustomizerWidthItem);
-
-        		// this doesn't work yet.
-        		jQuery("#customize-theme-controls ul").append(boomshakaCustomizerContainer);
-        		jQuery("#customize-theme-controls ul").append(boomshakaCustomizerTitle);
-        		jQuery("#customize-theme-controls ul").append(boomshakaCustomizerContentContainer);
-          };
-      });
+	  createPiece: function() { 
+	    var BoomShakaCustomizerContainer = document.createElement("li");
+	    BoomShakaCustomizerContainer.setAttribute("class", "control-section accordion-section top");
+	    BoomShakaCustomizerContainer.setAttribute("id", "accordion-section-title_boomshaka");
+	    var BoomShakaCustomizerTitle = document.createElement("h3");
+	    BoomShakaCustomizerTitle.appendChild(document.createTextNode("Customize Gallery"));
+	    
+	    var BoomShakaPieceContainer = document.createElement("ul");
+	    BoomShakaPieceContainer.setAttribute("accordion-section-content");
+	    BoomShakaPieceContainer.setAttribute("style", "display: block");
+	    
+	    // piece height
+	    var BoomShakaPieceHeightItem = document.createElement("li");
+	    BoomShakaPieceHeightItem.setAttribute("id", "customize-control-piece-default-height");
+	    BoomShakaPieceHeightItem.setAttribute("class", "customize-control customize-control-text");
+	    var BoomShakaPieceHeightLabel = document.createElement("label");
+	    BoomShakaPieceHeightLabel.appendChild(document.createTextNode("Piece Default Height"));
+	    
+	    // piece width
+  		var BoomShakaPieceWidthItem = document.createElement("li");
+  		BoomShakaPieceWidthItem.setAttribute("id", "customize-control-piece-default-width");
+  		BoomShakaPieceWidthItem.setAttribute("class", "customize-contol customize-control-text");
+  		var BoomShakaPieceWidthItemLabel = document.createElement("label");
+  		BoomShakaPieceWidthItemLabel.appendChild(document.createTextNode("Piece Default Width"));
+  		
+  		// piece weight (lbs)
+  		var BoomShakaPieceWeightItem = document.createElement("li");
+  		BoomShakaPieceWeightItem.setAttribute("id", "customize-control-piece-default-weight");
+  		BoomShakaPieceWeightItem.setAttribute("class", "customize-control customize-control-text");
+  		var BoomShakaPieceWeightItemLabel = document.createElement("label");
+  		BoomShakaPieceWeightItemLabel.appendChild(document.createTextNode("Piece Default Weight"));
+  		
+  		  		
+  		// append the boomshaka piece items to the container
+  		BoomShakaPieceContainer.appendChild(BoomShakaPieceHeightItem);
+  		BoomShakaPieceContainer.appendChild(BoomShakaPieceWidthItem);
+  		BoomShakaPieceContainer.appendChild(BoomShakaPieceWeightItem);
+  		
+  		// append piece container to the customizer
+  		BoomShakaCustomizerContainer.appendChild(BoomShakaPieceContainer);
+  		
+  		return BoomShakaCustomizerContainer;
 	  },
-	  Material: function() { 
+	  init: function(){
+	    return BoomShaka.createPiece();
+	  },
+	  _initializeFromDOM: function(domTree) { 
+	    // internal method, used to tail recurse out of initializeFromDOM
+	  },
+	  initializeFromDOM: function(domTree) { 
+	    for (elementName in domTree) { 
+	      while (domTree[elementName].hasChildNodes()) {
+	        this.initializeFromDom(domTree[elementName].firstChild);
+	      }
+	    }
+	  },
+    /* Boomshaka Material Design */
+	  Material: function(name, id, layer, width, height) { 
 	    // initial material design implementation
-	    this.content = null;
-	    this.device = null;
-	    return this;
-	  }
+	    // material constructor. Convert object to a material
+      // convert object to a material
+	    if (!document.getElementById(id)) { id = Math.floor(Math.random()*1000000000); }
+      return {type: "Material", name: name, id: id, layer: layer, width: width, height: height, x: 0, y: 0, z: 0, deviceid: null};
+	  },
 	};
 };
